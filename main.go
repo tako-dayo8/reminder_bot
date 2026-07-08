0b9caaac3936a79adba9842d166d0ad9fc32210e
tako1dayo <137975781+tako-dayo8@users.noreply.github.com>
2026-07-08 21:19:04 +0900

    change: setup logger & get token & init bot client & open discord session

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func main() {
-	fmt.Println("hello world")
+	// setup logger
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}))
+	slog.SetDefault(logger)
+
+	// get token
+	token := os.Getenv("DISCORD_BOT_TOKEN")
+	if len(token) == 0 {
+		slog.Error("Failed get token from env")
+		os.Exit(1)
+	}
+
+	// init bot client
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		slog.Error("Failed init discord bot client", "error", err)
+		os.Exit(1)
+	}
+
+	slog.Info("Successful init discord bot client")
+
+	// open discord session
+	if err := discord.Open(); err != nil {
+		slog.Error("Failed open discord session", "error", err)
+		os.Exit(1)
+	}
+
+	defer discord.Close()
 }
